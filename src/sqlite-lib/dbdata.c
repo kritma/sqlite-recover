10e497d760f0fd513dbc67acd743b173bd926437
kritma <silibakaboy@gmail.com>
2023-10-16 00:26:58 +0300

    fix native lib error

--- src/sqlite-lib/dbdata.c
+++ src/sqlite-lib/dbdata.c
@@ -934,7 +934,6 @@ static int sqlite3DbdataRegister(sqlite3 *db){
     0,                            /* xRelease */
     0,                            /* xRollbackTo */
     0,                            /* xShadowName */
-    0                             /* xIntegrity */
   };
 
   int rc = sqlite3_create_module(db, "sqlite_dbdata", &dbdata_module, 0);
